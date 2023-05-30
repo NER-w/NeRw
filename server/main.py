d758cf7bea20f674d5cbd3093b3af18fd4270d9e
oskar <oskartueuer@gmail.com>
2023-05-30 18:16:03 +0200

    added reqs.txt

--- server/main.py
+++ server/main.py
@@ -1,10 +1,5 @@
 from fastapi import FastAPI
+from myrouter import router
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-#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
+app.include_router(router)
